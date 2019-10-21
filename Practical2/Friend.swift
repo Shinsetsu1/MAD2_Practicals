dd9c1490683f32ebca7984128613ec9934ee86a1
Kheng Hin <jervisthelolicon@gmail.com>
2019-10-21 17:52:08 +0800

    eresf

--- Practical2/Friend.swift
+++ Practical2/Friend.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+class Friend {
+    var firstName:String = ""
+    var lastName:String = ""
+    var age:Int = 0
+    
+    var description:String = ""
+    init(firstname:String, lastname:String, age:Int) {
+        self.firstName = firstname
+        self.lastName = lastname
+        self.age = age
+    }
+}
--- Practical2/ViewController.swift
+++ Practical2/ViewController.swift
@@ -9,10 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var friendList: [Friend] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Exercise 1
+        let friend1 = Friend(firstname: "John", lastname: "Tan", age: 10)
+        let friend2 = Friend(firstname: "Tom", lastname: "Lim", age: 16)
+        let friend3 = Friend(firstname: "Jack", lastname: "Ong", age: 25)
+        let friend4 = Friend(firstname: "Harry", lastname: "Ng", age: 43)
+        let friend5 = Friend(firstname: "Wesley", lastname: "Qua", age: 5)
+        
+        friendList.append(friend1)
+        friendList.append(friend2)
+        friendList.append(friend3)
+        friendList.append(friend4)
+        friendList.append(friend5)
+        
+        //displayFriends()
+//        displayFriendsUnder20()
+    }
+    
+    // Exercise 2
+//    func displayFriends() {
+//        for friend in friendList {
+//            print("Name: \(friend.firstName)\(friend.lastName), Age:\(friend.age)")
+//        }
+//    }
+    
+    // Exercise 3
+//    func displayFriendsUnder20() {
+//        for friend in friendList {
+//            if (friend.age < 20) {
+//                print("Name: \(friend.firstName)\(friend.lastName)")
+//            }
+//        }
+//    }
+    
+    // Exercise 4
+    func findAverageAge()->Double {
+        var totalAge = 0
+        var averageAge: Double
+        for friend in friendList {
+            totalAge += friend.age
+            averageAge = Double(totalAge / friendList.count)
+        }
+        
     }
 
 
